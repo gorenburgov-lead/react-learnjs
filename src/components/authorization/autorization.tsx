45792e0b871b74dbd2bc37e09bfdc85c478a884b
Yuriy Gorenburgov <gorenburgov@gmail.com>
2026-05-01 16:39:34 +0300

    cr fix

--- src/components/authorization/autorization.tsx
+++ src/components/authorization/autorization.tsx
@@ -4,16 +4,12 @@ import { AuthorizationContext } from "../authorization-provider";
 export const Authorization = () => {
   const { name, isAuthorized, login, logout } =
     useContext(AuthorizationContext);
-  let output;
-  if (isAuthorized) {
-    output = (
-      <>
-        <span>{name}</span>
-        <button onClick={logout}>Logout</button>
-      </>
-    );
-  } else {
-    output = <button onClick={login}>Login</button>;
-  }
-  return output;
+  return isAuthorized ? (
+    <>
+      <span>{name}</span>
+      <button onClick={logout}>Logout</button>
+    </>
+  ) : (
+    <button onClick={login}>Login</button>
+  );
 };
